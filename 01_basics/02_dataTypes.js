e5b3e82cdf0ff47bf598365b5ba4f70da9dc52fc
Swarup <patilswarup110@gmail.com>
2025-07-05 16:53:13 +0000

    01 & 02

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol =>unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
